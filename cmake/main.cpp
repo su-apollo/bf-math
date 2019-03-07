256d802053ee66d135d7bd98aa8a8de29f89c601
apollohpp <apollo.hpp@gmail.com>
2019-03-07 23:49:35 +0900

    Add clion .idea directory.

---
+++ cmake/main.cpp
@@ -0,0 +1,19 @@
+#include "vector.hpp"
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!" << std::endl;
+
+    bf::float1 vec1(1.f);
+    bf::float2 vec2(1.f, 2.f);
+    bf::float3 vec3(1.f, 2.f, 3.f);
+    bf::float4 vec4(1.f, 2.f, 3.f, 4.f);
+
+    std::cout << vec1.x << std::endl;
+    std::cout << vec2.y << std::endl;
+    std::cout << vec3.z << std::endl;
+    std::cout << vec4.w << std::endl;
+
+    exit(EXIT_SUCCESS);
+}
